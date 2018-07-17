951a90162bf98ebd3e71c9b680cc8c9611499111
LuckySting <work.romanuel@yandex.ru>
2018-07-17 10:02:18 +0300

    added shelfs

--- mebelyes/api/models/Service.py
+++ mebelyes/api/models/Service.py
@@ -5,4 +5,7 @@ class Service(models.Model):
     description = models.CharField(max_length=256)
     price = models.CharField(max_length=256)
     slug = models.CharField(max_length=256)
+    def __str__(self):
+        return str(self.description)+" "+str(self.slug)
+
 
